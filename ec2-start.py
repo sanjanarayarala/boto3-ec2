a3e07b5f1e081c61f3be36a2924c5403174b8408
Sanjana Rayarala <srayarala@hawk.iit.edu>
2025-06-05 09:13:58 -0500

    Start an existing ec2 instance

---
+++ ec2-start.py
@@ -0,0 +1,12 @@
+import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+instance_id= os.getenv("Instance_IDs")
+
+aws_management_console= boto3.session.Session(profile_name='default')
+ec2_console= aws_management_console.client('ec2')
+
+#Start exisiting EC2 instance
+response= ec2_console.start_instances(InstanceIds= [instance_id])
